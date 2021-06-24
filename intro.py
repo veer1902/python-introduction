b6d9d46871b1e6fc23835d274b7481e60d89f045
veer1902 <78291988+veer1902@users.noreply.github.com>
2021-06-24 13:45:50 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,16 @@
+print("Hello world")
+fruit=["orange",7,"chair","table"]
+print(fruit[2])
+print (len(fruit))#number of items in fruit
+
+#to find data type
+print (type(fruit))
+print (type(2.9))
+print (type(45))
+print (type("veer"))
+print (type('A'))
+print (type(False))
+
+#take input from the user
+name = input("enter your name: ")
+print ("hello " +name)
